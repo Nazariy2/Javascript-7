83ab388aa5c3f2cef92e0ca8ca54ca7b0374772a
Nazariy2 <nazrud23@gmail.com>
2024-08-05 13:51:33 +0200

    a

---
+++ script.js
@@ -0,0 +1,68 @@
+// const number = [1, 2, 3,];
+// number [1] = 10;
+// console.log(number);
+
+// const string = ['faf', 'fafe', 'aewfrsh'];
+// string [3] = 'gwgsg';
+// console.log(string);
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let sum = 0;
+
+
+// for (let i = 0; i < numbers.length; i++) {
+ 
+//   sum += numbers[i];
+// }
+
+// console.log("Sum of the numbers:", sum);
+
+
+
+// const numbers = [10, 20, 30, 40, 50];
+
+// for (let i = 0; i < numbers.length; i++) {
+  
+//   console.log(numbers[i]);
+// }
+
+
+// const strings = [ "Hello", "JavaScript", "Code", "Programming", "Example"];
+  
+//   for (let i = 0; i < strings.length; i++) {
+   
+//     if (strings[i].length > 5) {
+      
+//       console.log(strings[i]);
+//     }
+//   }
+  
+
+// const numbers = [12, 45, 78, 34, 23, 89, 56, 98, 67, 54];
+
+
+// let max = numbers[0];
+
+// for (let i = 1; i < numbers.length; i++) {
+
+//   if (numbers[i] > max) {
+
+//     max = numbers[i];
+//   }
+// }
+
+// console.log("Max value in the array:", max);
+
+
+// const numbers = [12, 45, 78, 34, 23, 89, 56, 98, 67, 54];
+
+
+// for (let i = 0; i < numbers.length; i++) {
+
+//   if (numbers[i] % 2 === 0) {
+   
+//     console.log("pair numbers:", numbers[i]);
+//   }
+// }
+
